4d2dbd8aee443badbfe63132285887d5c6bcf5b5
StratoScripter <mhassanirshad1@gmail.com>
2023-10-27 10:06:48 -0400

    all test cases and function

--- service/routes.py
+++ service/routes.py
@@ -96,10 +96,10 @@ def get_accounts(account_id):
     ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
-        """
-        Update an Account
-        This endpoint will update an Account based on the posted data
-        """
+    """
+    Update an Account
+    This endpoint will update an Account based on the posted data
+    """
     app.logger.info("Request to update an Account with id: %s", account_id)
 
     account = Account.find(account_id)
