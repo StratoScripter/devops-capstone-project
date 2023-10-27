6c22e6abcbf0915d7d3b906337e2d0ea8a1c5af6
StratoScripter <mhassanirshad1@gmail.com>
2023-10-27 09:31:31 -0400

    first test case

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,23 +124,14 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
-    def test_read_an_account(self):
-    """It should Read an Account"""
-    # Create a new account and store its ID
-    response = self.client.post(BASE_URL, json=account_data)
-    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    account_id = response.get_json()["id"]
-
-    # Make a GET request to /accounts/{id}
-    response = self.client.get(f"{BASE_URL}/{account_id}")
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # Check that the returned JSON data matches the data sent earlier
-    account = response.get_json()
-    self.assertEqual(account["name"], account_data["name"])
-    self.assertEqual(account["email"], account_data["email"])
-    self.assertEqual(account["address"], account_data["address"])
-    self.assertEqual(account["phone_number"], account_data["phone_number"])
-    self.assertEqual(account["date_joined"], account_data["date_joined"])
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
 
 
