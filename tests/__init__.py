a4c5ed5118092b0b37d3304ed88d652da56e5375
StratoScripter <mhassanirshad1@gmail.com>
2023-10-28 10:20:46 +0500

    init.py update

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,12 @@
+from flask import Flask
 from flask_talisman import Talisman
+
+app = Flask(__name)
+
+# Initialize Flask-Talisman with your Flask app
 talisman = Talisman(app)
+
+# Other app configuration and routes...
+
+if __name__ == '__main__':
+    app.run()
