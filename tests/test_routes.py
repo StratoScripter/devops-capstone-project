26840276d83c836ab2bda3f3fc988ba9a1191c30
StratoScripter <mhassanirshad1@gmail.com>
2023-10-28 11:04:52 +0500

    talisman issue

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,6 +13,8 @@ from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
 from service import talisman
+from flask_talisman import Talisman
+
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
