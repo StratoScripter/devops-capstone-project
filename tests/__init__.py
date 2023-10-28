7ab62ba0923715f67a1b9c4560a0036a81c32dd1
StratoScripter <mhassanirshad1@gmail.com>
2023-10-28 11:50:45 +0500

    talisman issue 4

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_talisman import Talisman
-from flask import Flask
-
-
-# After the Flask app is created
-app = Flask(__name__)
-
-# Create an instance of the Talisman class
-talisman = Talisman(app)
