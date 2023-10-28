67b30a94ba6ae82f7d85e036d68ef46d1a8700a5
StratoScripter <mhassanirshad1@gmail.com>
2023-10-28 10:24:42 +0500

    init.py update

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,13 @@
-from flask import Flask
 from flask_talisman import Talisman
 
+# Create the Flask app
 app = Flask(__name)
 
 # Initialize Flask-Talisman with your Flask app
-talisman = Talisman(app)
-
-# Other app configuration and routes...
-
-if __name__ == '__main__':
-    app.run()
+talisman = Talisman(
+    app,
+    content_security_policy={
+        # ... other CSP directives ...
+        'frame-ancestors': "'self'",
+    },
+)
